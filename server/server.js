037bc50547f65c339d3f8b25a13c73ae82d90308
Nelluri Dolendra Sai Teja <dolendracse@gmail.com>
2026-05-13 00:55:41 +0530

    First version

--- server/server.js
+++ server/server.js
@@ -149,6 +149,14 @@ app.use(errorHandler);
 // Start server
 const PORT = process.env.PORT || 5000;
 
+process.on("uncaughtException", (err) => {
+  console.error("UNCAUGHT EXCEPTION:", err);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.error("UNHANDLED REJECTION:", err);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
